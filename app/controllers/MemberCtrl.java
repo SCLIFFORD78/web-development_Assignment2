e82327b446724b82be8fc0d9c3ba727cf23411d4
SCLIFFORD78 <faugh01@gmail.com>
2020-06-06 19:33:26 +0100

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/controllers/MemberCtrl.java
+++ app/controllers/MemberCtrl.java
@@ -44,13 +44,14 @@ public class MemberCtrl extends Controller
     Assessment assessment = new Assessment(weight, chest, thigh, upperArm, waist, hips, comments, dateToday());
     Member member = Member.findById(id);
     int pos = member.getAssessments().size();
-    member.setAssessments(member.getAssessments());
+
     member.getAssessments().add(assessment);
     //updates the trend icon status compareing assessment relative to eachother
 
     member.setBmi(GymUtility.calculateBMI(member,assessment));
     member.setStatus(GymUtility.determineBMICategory(GymUtility.calculateBMI(member, assessment)));
     member.setIsIdealBodyWeight(GymUtility.isIdealBodyWeight(member, assessment));
+    member.setAssessments(member.getAssessments());
     displayProgressByWeight(member);
     assessment.save();
     redirect ("/dashboard");
